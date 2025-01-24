62fafee3304c21d9af27db6a5950d761b38258f3
Amr <amrkhaled12338@gmail.com>
2025-01-24 21:19:19 +0200

    fix auth error page

--- app/(auth)/error/page.tsx
+++ app/(auth)/error/page.tsx
@@ -58,7 +58,7 @@ const errorMap: Record<
   [AuthError.CredentialsSignin]: {
     title: "Credentials Sign In Error",
     message: "There was an error signing in with credentials.",
-    action: "Check your credentials and try again",
+    action: "Check your credentials",
   },
   [AuthError.SessionRequired]: {
     title: "Session Required",
@@ -74,11 +74,11 @@ function AuthErrorContent() {
   const errorDetails = errorMap[error] || errorMap[AuthError.Default];
 
   return (
-    <div className="flex h-screen w-full flex-col items-center justify-center">
-      <div className="max-w-md rounded-lg bg-white p-8">
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <div className="">
         <div className="mb-6 text-center">
           <svg
-            className="mx-auto h-12 w-12 text-red-500"
+            className="mx-auto h-12 w-12 sm:h-[60px] sm:w-[60px] text-red-500"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
@@ -90,22 +90,21 @@ function AuthErrorContent() {
               d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
             />
           </svg>
-          <h2 className="mt-4 text-2xl font-bold text-gray-900">
+          <h2 className="mb-4 text-xl sm:text-3xl font-bold text-primary">
             {errorDetails.title}
           </h2>
-          <p className="mt-2 text-gray-600">{errorDetails.message}</p>
-          <code className="mt-2 block rounded-sm bg-gray-100 p-2 text-xs text-gray-700">
+          <p className="mt-2 text-muted-foreground text-center text-sm sm:text-base max-w-[300px] sm:max-w-[500px]">
+            {errorDetails.message}
+          </p>
+          <code className="mt-2 block rounded-sm bg-muted p-2 text-xs sm:text-sm text-muted-foreground w-full">
             Error Code: {error}
           </code>
         </div>
-        <div className="flex justify-center gap-4">
-          <Button
-            onClick={() => router.push("/login")}
-            className="bg-primary text-white hover:bg-primary/90"
-          >
+        <div className="flex justify-center gap-2">
+          <Button onClick={() => router.push("/login")} size="sm">
             {errorDetails.action}
           </Button>
-          <Button variant="outline" onClick={() => router.push("/")}>
+          <Button size="sm" variant="outline" onClick={() => router.push("/")}>
             Go Home
           </Button>
         </div>
