00f0ec96c4a14b1fcddbbb187c82ba11e67c1346
Amr <amrkhaled12338@gmail.com>
2025-04-18 13:44:58 +0200

    Remove sentry button from sanity studio

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "./styles/globals.css";
 import { ThemeProvider } from "@/components/Themes/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
 import { SpeedInsights } from "@vercel/speed-insights/next";
--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -9,6 +9,7 @@
 
 import { NextStudio } from "next-sanity/studio";
 import config from "../../../sanity.config";
+import "../../styles/studio-styles.css";
 
 export const dynamic = "force-static";
 
