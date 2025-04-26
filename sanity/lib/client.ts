fdf5611e51157d871a661515f451304229e7ad56
Amr <amrkhaled12338@gmail.com>
2025-04-26 18:41:13 +0300

    Set useCdn to true for faster portfolio data fetching

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -6,6 +6,6 @@ export const client = createClient({
   projectId,
   dataset,
   apiVersion,
-  useCdn: false, // Set to false if statically generating pages, using ISR or tag-based revalidation
+  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
   token,
 });
